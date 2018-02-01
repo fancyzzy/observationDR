9a6946cff6675202450a6163e802c0717eead2f6
fancyzzy <fancyzzy@163.com>
2018-02-01 21:10:25 +0800

    get col

--- gen_docx.py
+++ gen_docx.py
@@ -9,6 +9,7 @@ from collections import namedtuple
 import os
 
 from docx.enum.text import WD_ALIGN_PARAGRAPH
+import read_xlsx
 
 ProInfo = namedtuple("ProInfo", ['name', 'area', 'code', 'contract', 'builder',\
 		'supervisor', 'observor', 'xlsx_path', 'date'])
@@ -23,30 +24,51 @@ PAGE_CATEGORY = ['header', 'overview', 'environment', 'settlement_ground',\
 
 
 class MyDocx(object):
-	def __init__(self, docx_path, proj_info, data_source):
+	def __init__(self, docx_path, proj_info, my_xlsx):
 
 		self.proj = ProInfo(*proj_info)
 		self.docx = None
 		self.path = docx_path
+		self.date = proj_info[-1]
+		#xlsx实例
+		self.my_xlsx = my_xlsx
 
 	def gen_docx(self):
+		'''
+		生成docx文件
+		'''
 
 		if not self.path or not os.path.exists(self.path):
 			print("error, not an available path")
 			return
 		
 		self.docx = Document()
+
+		#创建首页
+		print("start making header pages")
 		if not self.make_header_pages():
 			print("DEBUG make_head_page error")
+		else:
+			pass
 
+		#创建数据分析页
+		print("start making overview pages")
+		if not self.make_overview_pages():
+			print("DEBUG make_overview_pages error")
+		else:
+			pass
+
+
+		print("Saving...")
 		self.docx.save(self.path)
-
 		return True
+	#######gen_docx()########################
 
-	def project_header(self, d):
+	def write_header(self):
 		'''
 		项目信息
 		'''
+		d = self.docx
 		d.add_paragraph("%s" % self.proj.name)
 
 		p = d.add_paragraph("施工单位: ")
@@ -100,7 +122,8 @@ class MyDocx(object):
 		d.add_page_break()
 		###page###########
 
-		self.project_header(d)
+		self.write_header()
+
 		d.add_paragraph("第三方检测审核单")
 		t = d.add_table(rows=1, cols=1, style = 'Table Grid')
 		t.cell(0, 0).text = "审核意见:\n\n\n\n\n" + " "*80 +"监理工程师:" + " "*30 + "日期:" 
@@ -111,12 +134,48 @@ class MyDocx(object):
 	##################make_header_pages()################	
 
 
+	def one_overview_table(self, area_name):
+		'''
+		一个区间的监测数据分析表
+		'''
+		d = self.docx
+		t = d.add_table(rows=1, cols=8, style='Table Grid')
+		t.cell(0,0).text = '监测项目'
+		t.cell(0,1).text = '本次\n变化\n最大点'
+		t.cell(0,2).text = '日变化\n速率\n(mm/d)'
+		t.cell(0,3).text = '日变量\n报警值\n(mm/d)'
+		t.cell(0,4).text = '累计\n变化量\n最大点'
+		t.cell(0,5).text = '累计\n变化量\n/mm'
+		t.cell(0,6).text = '累计\n变量\n报警值/mm'
+		t.cell(0,7).text = '累计\n变量\n控制值/mm'
+
+		#找到这个area的所有观测项目，作为首列内容
+		#all_areas_row_range = {'sheet1':{'area1':(1,10), 'area2':(11,15),...}, \
+		#'sheet2':{'area4':(1,23)}}
+		related_sheets = []
+		all_range = self.my_xlsx.all_areas_row_range
+		for sheet in self.my_xlsx.sheets:
+			if area_name in all_range[sheet].keys():
+				#还要考虑这一天的测量点有值:
+				pass
+				#related_sheets.append = [{sheet1:(1,10)},{sheet2:(3,15)}...]
+				related_sheets.append({sheet:all_range[sheet][area_name]})
+
+		print("DEBUG area: {}, related_sheets: {}".format(area_name,related_sheets))
+		for sheet in related_sheet:
+			pass
+
+
+		
+	##########one_overview_table()############
+
 	def make_overview_pages(self):
 		'''
 		监测数据分析表
 		'''
 		result = False
 		d = self.docx
+		areas = self.my_xlsx.areas
 
 		###page###########
 		d.add_page_break()
@@ -135,14 +194,23 @@ class MyDocx(object):
 
 		#表标题
 		table_cap = "监测数据分析表"
+		i = 0
+		for area_name in areas:
+			#test debug only one area
+			if '衡山路' in area_name:
+				i += 1
+				ss = '表' + '%d'%i + area_name + table_cap
+				d.add_paragraph(ss).paragraph_format.alignment = WD_ALIGN_PARAGRAPH.CENTER
+				self.one_overview_table(area_name)
 
-
+		result = True
+		return result
 
 	##################make_head_page()################	
 
 if __name__ == '__main__':
 
-
+	#测试
 	xlsx_path = r'C:\Users\tarzonz\Desktop\oreport\一二工区计算表2018.1.1.xlsx' 
 	project_info = ["青岛市地铁1号线工程", "一、二工区", "DSFJC02-RB-594", "M1-ZX-2016-222", \
 	"中国中铁隧道局、十局集团有限公司", "北京铁城建设监理有限责任公司",\
@@ -154,9 +222,10 @@ if __name__ == '__main__':
 		pass
 
 	data_source = r'C:\Users\tarzonz\Desktop\oreport\一二工区计算表2018.1.1.xlsx'
-
-	my_docx = MyDocx(docx_path, project_info, data_source)
+	my_xlsx = read_xlsx.MyXlsx(xlsx_path)
+	my_docx = MyDocx(docx_path, project_info, my_xlsx)
 	res = my_docx.gen_docx()	
+
 	if res:
 		print("'{}' has been created".format(docx_path))
 		print("Done")
--- read_xlsx.py
+++ read_xlsx.py
@@ -6,40 +6,36 @@
 获取每个sheet的所有区间的名字和行范围
 '''
 import openpyxl
+from datetime import datetime
 
 
 class MyXlsx(object):
 	def __init__(self, xlsx_path):
 		self.path = xlsx_path
 
-		print("DEBUG, start to load datasource...")
-		self.xlsx = openpyxl.load_workbook(xlsx_path)
-		print("DEBUG, load finished.")
+		print("init to load datasource...")
+		self.wb = openpyxl.load_workbook(xlsx_path)
+		print("xlsx load finished.")
 
 		#获得所有的sheet页名单, 即观测项目
-		self.sheets_list =  self.xlsx.sheetnames[:]
-		print("DEBUG observation_list=", self.sheets_list)
+		self.sheets =  self.wb.sheetnames[:]
 
-		sheet_ranges = self.xlsx['地表沉降']
-		print("DEBUG sheet_ranges", sheet_ranges)
-		print("DEBUG sheet_ranges B3", sheet_ranges['B3'].value)
-
-		#test one sheet_area_range
-		ground_sheet_areas = self.get_one_sheet_areas_range('地表沉降')
-		print("DEBUG test, ground_area: ", ground_sheet_areas)
+		#预先保存所有sheet的最大列数
+		self.d_maxcol = {}
+		for sheet in self.sheets:
+			self.d_maxcol[sheet] = len(tuple(self.wb[sheet].columns))
 
 
-		'''
 		#获取所有sheet的区间的行范围, 以字典形式为数据索引
 		#{'sheet1':{'area1':(1,10), 'area2':(11,15),...}, 'sheet2':{'area4':(1,23)}}
-		self.all_row_range = self.get_all_sheets_areas_range()
+		self.all_areas_row_range = self.get_all_sheets_areas_range()
+
 		#获得'地表沉降'页的A列元素, 作为总区间汇总
 		#['area1','area2','area3',...'area8']
 		#疑问，是否可以用地表沉降的区间作为全部区间?
 		sheet_name = '地表沉降'
-		d_areas = self.all_row_range(sheet_name)
-		self.total_areas = list(d_areas.keys())
-		'''
+		d_areas = self.all_areas_row_range[sheet_name]
+		self.areas = list(d_areas.keys())
 	##################__init__()##############################	
 
 
@@ -53,7 +49,7 @@ class MyXlsx(object):
 		all_sheets_areas_range = {}
 		sheet_areas_range = {}
 
-		for sheet in self.sheets_list:
+		for sheet in self.sheets:
 			sheet_areas_range = self.get_one_sheet_areas_range(sheet)
 			all_sheets_areas_range[sheet] = sheet_areas_range
 
@@ -70,29 +66,25 @@ class MyXlsx(object):
 		'''
 		sheet_areas_range = {}
 		area_name = ''
-		print("DEBUG get_one_sheet_areas_range start, sheet_name=", sheet_name)
-		sheet = self.xlsx[sheet_name]
-		print("DEBUG sheet got: ", sheet)
-		print("DEBUG sheet got type: ", type(sheet))
+		sheet = self.wb[sheet_name]
 		start = 0
 		start_count = False
 		#最大支持500行的观测点个数
 		for i in range(1, 500):
 			#表格格式注意, 区间必须是在A列, A列开始为空
 			v_1_col = sheet.cell(row=i, column=1).value
-			print("DEBUG i:{}, v_1_col:{}".format(i, v_1_col))
+			#print("DEBUG i:{}, v_1_col:{}".format(i, v_1_col))
 			v_2_col = sheet.cell(row=i, column=2).value
 			if v_1_col != None and (not start_count):
 				area_name = v_1_col
-				print("DEBUG found an area area_name=", v_1_col)
+				#print("DEBUG found an area area_name=", v_1_col)
 				start = i
 				start_count = True
 
 			#发现新的area, 保存之前area的name,和上一行的行号i-1
 			elif v_1_col != None and start_count:
 				sheet_areas_range[area_name] = (start,i-1)
-				print("DEBUG added an area{},({})".format(area_name, (start,i-1)))
-				print("DEBUG total:",sheet_areas_range)
+				#print("DEBUG added an area{},({})".format(area_name, (start,i-1)))
 				#start 重新开始记录
 				area_name = v_1_col
 				start = i
@@ -101,20 +93,46 @@ class MyXlsx(object):
 			#表格格式注意, 观测点之间不能有空行
 			elif v_1_col == None and v_2_col == None and start_count:
 				sheet_areas_range[area_name] = (start,i-1)
-				print("DEBUG added an area{},({})".format(area_name, (start,i-1)))
-				print("DEBUG total:",sheet_areas_range)
+				#print("DEBUG added an area{},({})".format(area_name, (start,i-1)))
 				break
 
 			else:
 				#继续寻找
 				continue
 
-		print("DEBUG sheet '{}' has areas_range: {}".format(\
-			sheet_name, sheet_areas_range))
-
 		return sheet_areas_range
 	#######get_one_sheet_areas_range()###########################
 
+
+	def get_col(self, sheet, val):
+		'''
+		通过一个日期，或者列字符，
+		找得这个值所在的列坐标
+		'''
+		#从后往前找
+		sh = self.wb[sheet]
+		for i in range(self.d_maxcol[sheet], 0, -1):
+
+			#查找前两排，找到这个值，返回这个值的列坐标
+			#表格格式注意，日期用日期格式，python里面是
+			#datetime.datetime类型
+			#每个sheet的行表头在row1和row2
+			if val == sh.cell(1,i).value or val == sh.cell(2,i).value:
+				print("find! i=", i)
+				return i
+
+		return None
+
+	#########get_col()##########################################
+
+
+	def get_values(self, sheet, area_name, col):
+		'''
+		通过sheet，area和列
+		返回area的这列的值[]
+		'''
+		pass
+	#########get_values()######################################
 
 
 if __name__ == '__main__':
@@ -124,7 +142,32 @@ if __name__ == '__main__':
 
 	my_xlsx = MyXlsx(xlsx_path)
 
+	#测试获得所有列的数目
+	#all_column = my_xlsx.wb['地表沉降'].columns
+	#print("DEBUG all_row=",len(all_row))
+	#print("DEBUG all_col=",len(tuple(all_column)))
+
 	#print("DEBUG ground_sheet_areas=",my_xlsx.total_areas)
+
+
+
+	#测试找某一个日期的列坐标
+	ws = my_xlsx.wb['地表沉降']
+
+	ss = '2018/1/1'
+	sd = datetime.strptime(ss, '%Y/%m/%d')
+	print("DEBUG sd=",sd)
+
+	i = my_xlsx.get_col('地表沉降', sd)
+	print("i=",i)
+
+	'''
+	#获取某一个单元格的值
+	dd = ws['WE2'].value
+	ddd = ws.cell(2, 603).value
+	print("Debug ddd=",ddd)
+	print("DEBUG type(ddd)=",type(ddd))
+	'''
 
 
 
